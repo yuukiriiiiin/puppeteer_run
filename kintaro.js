a3afb748ab3103f823697c3ebbc43fa753d5db78
yuki-sakata <yuki_sakata@waku-2.com>
2021-02-02 18:07:38 +0900

    add: commandline arg

--- kintaro.js
+++ kintaro.js
@@ -9,9 +9,9 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://kintarou.r.recruit.co.jp/Lysithea/JSP_Files/authentication/WC010_1.jsp?dsname=lib.IASSTORE&PERSON_CODE=01039888&FROM=WC010_2');
   // ユーザーID
-  await page.type('input[name="PersonCode"]', "");
+  await page.type('input[name="PersonCode"]', process.argv[2]);
   // パスワード
-  await page.type('input[name="Password"]', "");
+  await page.type('input[name="Password"]', process.argv[3]);
   await page.click('input[type="submit"]');
   await Promise.all([
     page.waitForNavigation()
@@ -21,10 +21,11 @@ const puppeteer = require('puppeteer');
   await page.click('.today');
   await page.waitForSelector('#bar3_1000')
   await page.click('#bar3_1000')
-  await page.waitForSelector('#endTimeD')
+  await page.waitForSelector('#startTimeD, #endTimeD')
+  await page.$eval("#startTimeD", element => element.value = '')
   await page.$eval("#endTimeD", element => element.value = '')
-  // 時間は任意で設定
-  await page.type("#endTimeD", "1930")
+  await page.type("#startTimeD", process.argv[4])
+  await page.type("#endTimeD", process.argv[5])
   await page.click("#importBtn")
   const buttonTagAll = await page.$$('button');
   let tagText = [];
